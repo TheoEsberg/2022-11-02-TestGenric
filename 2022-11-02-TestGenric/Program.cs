d4498dd7f81a3358c73f59cf7306be655d128191
Theo Esberg <esbergtheo@gmail.com>
2022-11-02 13:27:03 +0100

    Add project files.

---
+++ 2022-11-02-TestGenric/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2022_11_02_TestGenric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool test1 = TestGeneric.Compare(1, 2);
+            bool test2 = TestGeneric.Compare(2, 2);
+            bool test3 = TestGeneric.Compare("hej", "hej");
+            bool test4 = TestGeneric.Compare("Hej", "HEJ");
+
+            Console.WriteLine("Första värdet är {0}", test1);
+            Console.WriteLine("Andra värdet är {0}", test2);
+            Console.WriteLine("Tredje värdet är {0}", test3);
+            Console.WriteLine("Fjärde värdet är {0}", test4);
+
+        }
+    }
+}
---
+++ 2022-11-02-TestGenric/TestGeneric.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2022_11_02_TestGenric
+{
+    public class TestGeneric
+    {
+        public static bool Compare<T>(T value1, T value2) { return (value1.Equals(value2)); }
+    }
+}
